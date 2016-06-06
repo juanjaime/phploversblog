2dde5d5dcfbfdec0f168fae3fcba749adfb85f51
Juan Jaime <juanjaime.montemayor@gmail.com>
2016-06-05 20:47:17 -0500

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juan
+ * Date: 5/06/16
+ * Time: 08:36 PM
+ */
---
+++ libraries/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: juan
+ * Date: 5/06/16
+ * Time: 08:39 PM
+ */
+class Database
+{
+
+}
